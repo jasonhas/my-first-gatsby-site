3e862a9727fb77ce6a47968ba14664847eeac0b9
Jason Hasselkus <jhasselkus@opentext.com>
2022-12-09 15:25:42 -0800

    Finished FileSystem part

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
+    siteUrl: `https://www.somethinggreat.com`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Little puppy dog"
+        src="../images/white-puppy.jpg"
+        />
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page"></Seo>
 
 export default IndexPage
